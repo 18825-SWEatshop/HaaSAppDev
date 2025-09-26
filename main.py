30e5abc2906a2a844fa0fd81e0f771a9bf7b0c37
Griffin Prince <princegriffin17@gmail.com>
2025-09-26 10:37:15 -0500

    organization & jwt token creation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import jwt
+
 
 app = FastAPI()
 
@@ -14,6 +16,8 @@ def login(request: LoginRequest):
     # TODO: Replace with real authentication 
     
     if request.username == "admin" and request.password == "admin":
-        return {"message": "Login successful"}
+        # Generate JWT token 
+        token = jwt.encode({"username": request.username}, "this-is-a-super-secret-key-nobody-will-guess-this-key-duck", algorithm="HS256")
+        return {"message": "Login successful", "token": token}
     else:
         raise HTTPException(status_code=401, detail="Invalid credentials")
